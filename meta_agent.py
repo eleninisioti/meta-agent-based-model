f1609761fa09c336621efa6d0aeb8dc51999a07f
Eleni <e.nisioti@essex.ac.uk>
2022-07-02 17:36:29 +0200

    initialized

---
+++ meta_agent.py
@@ -0,0 +1,109 @@
+import os
+from tournament import Tournament
+import pickle
+from visualize import plot_grid
+import yaml
+import numpy as np
+import random
+
+
+class MetaAgent:
+
+    def __init__(self, args, idx, metastrategy):
+        self.metastrategy = metastrategy
+        if self.metastrategy == "rounds":
+            self.strategy = 1
+        elif self.metastrategy == "benefit":
+            self.strategy = 0.2
+        self.idx = idx
+        self.args = args
+        self.agent_project_dir = "../projects/" + args.project + "/agent_" + str(idx)
+
+        if not os.path.exists(self.agent_project_dir + "/plots/grids"):
+            os.makedirs(self.agent_project_dir + "/plots/grids")
+
+
+
+        self.log_experiments = []
+        self.memory = 50
+
+    def simulate(self):
+        # initialize project's subdirs
+        log_perf = {"coop_perc": []}
+
+        if self.args.metastrategy == "rounds":
+            rounds = self.strategy
+        elif self.args.metastrategy == "benefit":
+            self.args.benefit = self.strategy
+            rounds = self.args.rounds
+
+        tournament = Tournament(self.args)
+        logs = []
+
+        for round in range(rounds):
+            log_round = tournament.play_round()
+            plot_grid(strat_transitions=log_round["strat_transitions"], round=round, project=self.agent_project_dir)
+            logs.append(log_round)
+            pop_log = tournament.pop_log()
+            log_perf["coop_perc"].append(pop_log["coop_perc"])
+
+        # ----- final saving for project ------
+
+        with open(self.agent_project_dir + '/log.pickle', 'wb') as pfile:
+            pickle.dump(log_perf, pfile, protocol=pickle.HIGHEST_PROTOCOL)
+
+        self.current_experiment = log_round["strat_transitions"]
+        if len(self.log_experiments) < self.memory:
+            self.log_experiments.append(log_round["strat_transitions"])
+        else:
+            self.log_experiments = [log_round["strat_transitions"]]
+
+    def compute_fitness(self):
+        # novelty
+        novelty = 0
+        for el in self.log_experiments[:-1]:
+            diff = np.sum(el == self.current_experiment)
+            novelty += diff
+        novelty = novelty/len(self.log_experiments)
+        novelty = novelty/(len(self.current_experiment)*len(self.current_experiment[0]))
+        # pattern-recognition (static)
+        # are there cooperators?
+        num_coops = (self.current_experiment == 0).sum()
+        num_coops += (self.current_experiment == 2).sum()
+
+        num_coops = num_coops/(len(self.current_experiment)*len(self.current_experiment[0]))
+        # are they clustered?
+        flattened = self.current_experiment.flatten()
+        pos = np.where(flattened == 0)[0]
+        coops = np.ndarray.tolist(pos)
+        coops.extend(np.ndarray.tolist(np.where(flattened == 2)[0]))
+        clusters = []
+        for el1 in coops:
+            cluster = 0
+            for el2 in coops:
+                if np.abs(el1 - el2) == 1:
+                    cluster += 1
+
+            clusters.append(cluster)
+        if len(coops):
+            clustering = np.mean([el/len(coops) for el in clusters])
+        else:
+            clustering = 0
+        self.fitness = novelty + num_coops + clustering
+        print("agent", self.idx, novelty, num_coops, clustering)
+
+    def update_strategy(self):
+        max_fitness = self.fitness
+        for neighbor in self.neighbors:
+            if neighbor.fitness > max_fitness:
+                max_fitness = neighbor.fitness
+                self.strategy = neighbor.strategy
+
+        # mutate strategy
+        if self.metastrategy == "rounds":
+            mutation = random.randint(-1,2)
+            self.strategy = np.max([1, np.abs(self.strategy + mutation)])
+        elif self.metastrategy == "benefit":
+            mutation = np.random.uniform(low=0.1, high=0.3, size=1)[0]
+            self.strategy = np.max([0, np.abs(self.strategy + mutation)])
+
---
+++ meta_network.py
@@ -0,0 +1,205 @@
+import argparse
+import networkx as nx
+from meta_agent import MetaAgent
+from visualize import plot_meta_model
+import numpy as np
+import os
+import yaml
+import pickle
+
+def init_structure(shape, n_agents,  n_neighbors=1):
+    """ Initializes the social network topology.
+
+    Params
+    ------
+    shape: str
+        the social network topology
+
+    n_agents: int
+        number of agents
+
+    """
+    if shape == "ring":
+        structure = nx.cycle_graph(n_agents)
+
+    elif shape == "fully-connected":
+        structure = nx.complete_graph(n_agents)
+
+    elif shape == "small-world":  # small-world
+        structure = nx.watts_strogatz_graph(n=n_agents, k=n_neighbors, p=0.2)
+
+    return structure
+
+
+def evolve(agents):
+
+    for agent in agents:
+        agent.simulate()
+        agent.compute_fitness()
+
+    for agent in agents:
+        agent.update_strategy()
+
+    return agents
+
+def main(args):
+    project_dir = "../projects/" + args.project
+    with open(project_dir + '/config.yaml', 'w') as outfile:
+        yaml.dump(args, outfile)
+
+    for trial in range(args.trials):
+        project_dir = "../projects/" + args.project + "/trial_" + str(trial) + "/plots"
+        if not os.path.exists(project_dir):
+            os.makedirs(project_dir)
+
+        # initialize network structure
+        structure = init_structure(args.structure, args.num_meta_agents)
+        agents = []
+
+        for n in range(args.num_meta_agents):
+            agents.append(MetaAgent(args, idx=n, metastrategy=args.metastrategy))
+
+        for i, agent in enumerate(agents):
+            neighbor_idxs = list(structure.neighbors(i))
+            agent.neighbors = [agents[n] for n in neighbor_idxs]
+
+        for round in range(args.metarounds):
+            print("round", round)
+            agents = evolve(agents)
+            keep_log(agents)
+
+        with open(project_dir + '/log.pkl', 'wb') as outfile:
+            pickle.dump(log, outfile)
+
+
+        plot_meta_model(log, project_dir)
+
+def keep_log(agents):
+    # average fitness
+    log["fitness"].append(np.mean([agent.fitness for agent in agents]))
+
+    # histogram of strategies
+    log["strategies"].append( [agent.strategy for agent in agents])
+
+if __name__ == "__main__":
+    log = {"fitness": [], "strategies": []}
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--metastrategy',
+                        help='Meta strategy. Choose between rounds and benefit',
+                        type=str,
+                        default="rounds")
+
+    parser.add_argument('--structure',
+                        help='Type of social structure',
+                        type=str,
+                        default="fully-connected")
+
+    parser.add_argument('--num_meta_agents',
+                        help='Number of meta-agents',
+                        type=int,
+                        default=20)
+
+    parser.add_argument('--project',
+                        help='Name of current project',
+                        type=str,
+                        default="temp")
+
+    parser.add_argument('--order',
+                        help='Choose between CDO (combat-diffusion-offspring) and COD ('
+                             'combat-offspring-diffusion).',
+                        type=str,
+                        default="COD")
+
+    parser.add_argument('--game',
+                        help='Name of game. Choose between PD and Snow',
+                        type=str,
+                        default="PD")
+
+    parser.add_argument('--grid_length',
+                        help='Length of grid in tiles ',
+                        type=int,
+                        default=20)
+
+    parser.add_argument('--radius',
+                        help='Neighborhood radius ',
+                        type=int,
+                        default=1)
+
+    parser.add_argument('--cost',
+                        help='Cost of cooperation.',
+                        type=float,
+                        default=0)
+
+    parser.add_argument('--benefit',
+                        help='Benefit of cooperation.',
+                        type=float,
+                        default=10)
+
+    parser.add_argument('--inter_per_round',
+                        help='Interactions per round.',
+                        type=int,
+                        default=8)
+
+    parser.add_argument('--init_coop',
+                        help='Initial percentage of cooperators.',
+                        type=float,
+                        default=0.1)
+
+    parser.add_argument('--prob_move',
+                        help='Probability of moving during the day time.',
+                        type=float,
+                        default=0.1)
+
+    parser.add_argument('--rounds',
+                        help='Number of evolutionary rounds.',
+                        type=int,
+                        default=20)
+
+    parser.add_argument('--metarounds',
+                        help='Number of meta rounds.',
+                        type=int,
+                        default=30)
+
+    parser.add_argument('--trials',
+                        help='Number of independent trials.',
+                        type=int,
+                        default=10)
+
+    parser.add_argument('--day_duration',
+                        help='Number of trials a day consists of',
+                        type=int,
+                        default=5)
+
+    parser.add_argument('--night_duration',
+                        help='Number of trials a night consists of',
+                        type=int,
+                        default=5)
+
+    parser.add_argument('--nagents',
+                        help='Number of agents',
+                        type=int,
+                        default=10)
+
+    parser.add_argument('--well_mixed',
+                        help='Number of evolutionary rounds.',
+                        default=False,
+                        action="store_true")
+
+    parser.add_argument('--bifurcation',
+                        help='Whether a bifurcation plot will be made.',
+                        default=False,
+                        action="store_true")
+
+    parser.add_argument('--eval_movement',
+                        help='Evaluate movement during daytime.',
+                        default=False,
+                        action="store_true")
+
+    parser.add_argument('--move_parametric',
+                        help='Evaluate different values of movement.',
+                        default=False,
+                        action="store_true")
+
+    args = parser.parse_args()
+    main(args)
